4ddab6655bd76bd27a738b733026a54a7d28b0e5
Hacker-From-Fallout <chernyshovignat87@gmail.com>
2024-06-29 10:28:21 +0300

    Delete src directory

--- src/components/index.js
+++
@@ -1,9 +0,0 @@
-export * from "./TagsBlock";
-export * from "./CommentsBlock";
-export * from "./CommentsBlockHome";
-export * from "./Post";
-export * from "./AddComment";
-export * from "./SideBlock";
-export * from "./UserInfo";
-export * from "./Header";
-
